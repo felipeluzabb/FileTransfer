ce1bd04bb683d3c04ed2dbeaef2212bb4a879f46
Felipe Luz <felipeluzabb@gmail.com>
2024-06-10 10:39:24 -0300

    initial commit

---
+++ FileTransfer/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTransfer
+{
+    public class Data
+    {
+        public List<string> GetFileNameArray()
+        {
+            var array = new List<string>();
+
+            array.Add("test.txt");
+            
+            return array;
+        }
+    }
+}
---
+++ FileTransfer/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTransfer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Transferência iniciada em " + DateTime.Now);
+                var array = new Data().GetFileNameArray();
+
+                foreach (var item in array)
+                {
+                    string fileName = item;
+                    string sourcePath = @"E:\Documents";
+                    string targetPath = @"E:\Documents\SubFolder";
+
+                    // Use Path class to manipulate file and directory paths.
+                    string sourceFile = System.IO.Path.Combine(sourcePath, fileName);
+                    string destFile = System.IO.Path.Combine(targetPath, fileName);
+                    System.IO.File.Copy(sourceFile, destFile, true);
+
+                    Console.WriteLine("Arquivo " + fileName + " transferido com sucesso");
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Transferência(s) realizada(s) com sucesso!");
+                Console.WriteLine("Pressione qualquer tecla para fechar.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
